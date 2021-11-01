5a07b715be46ec2d84d06ba314830791d0ee493a
unknown <GopalKadam>
2021-11-01 16:54:26 +0530

    ManyToManyProject

---
+++ ProjectManytoMany/src/main/java/com/profinch/ProjectManytoMany/ProjectManytoManyApplication.java
@@ -0,0 +1,13 @@
+package com.profinch.ProjectManytoMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManytoManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManytoManyApplication.class, args);
+	}
+
+}
---
+++ ProjectManytoMany/src/test/java/com/profinch/ProjectManytoMany/ProjectManytoManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profinch.ProjectManytoMany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManytoManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
